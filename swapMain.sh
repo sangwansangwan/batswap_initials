694bc259763eb36cafadaaf59ae480aab56f5110
sangwanritik <ritik.sangwan@email.com>
2024-02-07 15:12:57 +0530

    added little prettyness

--- swapMain.sh
+++ swapMain.sh
@@ -2,8 +2,18 @@
 
 process_name="stationRpi"
 chmod +x stationRpi
-script_path='.'  # provide the name of the folder
 
+# Provide the name of the variables down below --------------
+script_path='.'         # absolute path where script is located
+stationId=21             
+rackId=18               
+organisationId="electrifuel"
+portNumber=1
+#--------------------------------------------------------------
+
+
+
+#------------------- Do not edit below this part ----------------------
 current_date=$(date +'%Y-%m-%d')
 
 logs_dir="$script_path/logs"
@@ -15,12 +25,13 @@ check_process() {
     local process_count=$(ps aux | grep "$process_name" | wc -l)
 
     if [ "$process_count" -gt 2 ]; then
-        echo "Process swapping is running." $process_count
+        echo "Process swapping is already running." $process_count
         sudo pkill -f "$process_name"
     fi
+
     if [ "$process_count" -lt 2 ]; then
         echo "Process started..."
-        $script_path/stationRpi -oid electrifuel -sid 21 -rid 18 -p 2 >> "$log_file" 2>&1 &
+        $script_path/stationRpi -oid ${organisationId} -sid ${stationId} -rid ${rackId} -p ${portNumber} >> "$log_file" 2>&1 &
     fi
 }
 
@@ -28,3 +39,5 @@ while true; do
     check_process
     sleep 25
 done
+#------------------- Do not edit above this part ----------------------
+
--- swapPre.sh
+++ swapPre.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# logic to add chromium to autostart -------------->
-file="/etc/xdg/lxsession/LXDE-pi/autostart"
 
+# --------------- Part to be edited before execution -------------------->
+cron_command=""  # Provide absolute path of swapMain.sh
 string="@chromium --kiosk http://batswap.electrifuel.com:3000"
 #string="@chromium-browser --kiosk http://batswap.electrifuel.com:3000"
 # Uncomment above line and comment line above that if chromium process name is chromium-broswer
+# --------------- Part to be edited before execution -------------------->
 
+
+# logic to add chromium to autostart----------------> 
+file="/etc/xdg/lxsession/LXDE-pi/autostart"
 if ! grep -qF "$string" "$file"; then
     sudo sh -c "echo '\n$string' >> '$file'"
 fi
 # -------------------------------------------------->
 
 
-
 # logic to add cronjob after checking its existence---->
 cron_schedule="@reboot"
-cron_command=""  # Provide absolute path of swapMain.sh
-
 existing_cron=$(crontab -l 2>/dev/null | grep -F "$cron_command")
 if [ -z "$existing_cron" ]; then
     (crontab -l 2>/dev/null; echo "$cron_schedule $cron_command") | crontab -
