3a61a961aaf49dc47e92295e21bb53adb581daee
sangwanritik <ritik.sangwan@email.com>
2024-02-12 14:27:11 +0530

    fixed date file change logic

--- swapMain.sh
+++ swapMain.sh
@@ -8,21 +8,27 @@ script_path='.'         # absolute path where script is located
 stationId=21             
 rackId=18               
 organisationId="electrifuel"
-portNumber=1
+portNumber=0
 #--------------------------------------------------------------
 
 
 
 #------------------- Do not edit below this part ----------------------
-current_date=$(date +'%Y-%m-%d')
-
-logs_dir="$script_path/logs"
-mkdir -p "$logs_dir"
-
-log_file="$logs_dir/log_$current_date.txt"
-
 check_process() {
+
+    current_date=$(date +'%Y-%m-%d')
+
+    logs_dir="$script_path/logs"
+    mkdir -p "$logs_dir"
+
+    log_file="$logs_dir/log_$current_date.txt"
+
+    echo $log_file
     local process_count=$(ps aux | grep "$process_name" | wc -l)
+
+    if [ ! -f "$log_file" ]; then
+        sudo pkill -f "$process_name"
+    fi
 
     if [ "$process_count" -gt 2 ]; then
         echo "Process swapping is already running." $process_count
